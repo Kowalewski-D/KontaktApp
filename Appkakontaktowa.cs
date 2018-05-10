f5c61a469276c663c0cdfcec8b6639181fdfb66f
Kowalewski-D <39136715+Kowalewski-D@users.noreply.github.com>
2018-05-10 02:01:51 +0200

    Add files via upload

---
+++ Appkakontaktowa.cs
@@ -0,0 +1,142 @@
+﻿using Apkakontaktowa.Kontaktklasy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appkakontaktowa
+{
+    public partial class Appkakontaktowa : Form
+    {
+        public Appkakontaktowa()
+        {
+            InitializeComponent();
+        }
+
+
+        kontaktclass c = new kontaktclass(ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString);
+
+        private void dodajbutt_Click(object sender, EventArgs e)
+        {
+            c.imie = imietxtbox.Text;
+            c.nazwisko = nazwiskotxtbox.Text;
+            c.telefon = telefontxtbox.Text;
+            c.adres = adrestxtbox.Text;
+            c.plec = pleccombobox.Text;
+
+            bool success = c.dodaj();
+            if (success == true)
+            {
+                MessageBox.Show("Poprawnie dodano kontakt");
+                wyczysc();
+            }
+            else
+            {
+                MessageBox.Show("Nie dodano poprawnie kontaktu. Spóbuj ponownie ");
+            }
+            DataTable dt = c.Select();
+            dgvListakontaktow.DataSource = dt;
+        }
+
+
+        private void Appkakontaktowa_Load(object sender, EventArgs e)
+        {
+            DataTable dt = c.Select();
+            dgvListakontaktow.DataSource = dt;
+        }
+        //metoda czyszczenia textboxow
+        public void wyczysc()
+        {
+           
+            imietxtbox.Text = "";
+            nazwiskotxtbox.Text = "";
+            telefontxtbox.Text = "";
+            adrestxtbox.Text = "";
+            pleccombobox.Text = "";
+            kontaktidtxtbox.Text = "";
+        }
+
+        private void aktualizujbutt_Click(object sender, EventArgs e)
+        {
+            c.kontaktID = int.Parse(kontaktidtxtbox.Text);
+            c.imie = imietxtbox.Text;
+            c.nazwisko = nazwiskotxtbox.Text;
+            c.telefon = telefontxtbox.Text;
+            c.adres = adrestxtbox.Text;
+            c.plec = pleccombobox.Text;
+
+            bool success = c.aktualizuj();
+            if (success == true)
+            {
+                MessageBox.Show("Poprawnie zaktualizowano kontakt");
+
+                DataTable dt = c.Select();
+                dgvListakontaktow.DataSource = dt;
+
+                wyczysc();
+            }
+            else
+            {
+                MessageBox.Show("Nie poprawnie zaktualizowano kontakt. Spóbuj ponownie ");
+            }
+
+        }
+
+        private void dgvListakontaktow_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+            //wrzucanie z database grid do poszczegolnych danych do poszczegolnych textboxow
+            int rowIndex = e.RowIndex;
+            kontaktidtxtbox.Text = dgvListakontaktow.Rows[rowIndex].Cells[0].Value.ToString();
+            imietxtbox.Text = dgvListakontaktow.Rows[rowIndex].Cells[1].Value.ToString();
+            nazwiskotxtbox.Text = dgvListakontaktow.Rows[rowIndex].Cells[2].Value.ToString();
+            telefontxtbox.Text = dgvListakontaktow.Rows[rowIndex].Cells[3].Value.ToString();
+            adrestxtbox.Text = dgvListakontaktow.Rows[rowIndex].Cells[4].Value.ToString();
+            pleccombobox.Text = dgvListakontaktow.Rows[rowIndex].Cells[5].Value.ToString();
+
+        }
+
+        private void wyczyscbutt_Click(object sender, EventArgs e)
+        {
+            wyczysc();
+        }
+
+        private void usunbutt_Click(object sender, EventArgs e)
+        {
+            c.kontaktID = Convert.ToInt32(kontaktidtxtbox.Text);
+            bool success = c.usun();
+            if(success == true)
+            {
+                MessageBox.Show("Kontakt poprawnie usuniety");
+
+                DataTable dt = c.Select();
+                dgvListakontaktow.DataSource = dt;
+
+                wyczysc();
+            }
+            else
+            {
+                MessageBox.Show("Kontakt niepoprawnie usuniety. Spróbuj ponownie");
+            }
+        }
+        static string myconnstr = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        private void szukajtxtbox_TextChanged(object sender, EventArgs e)
+          {
+            string keyword = szukajtxtbox.Text;
+            SqlConnection conn = new SqlConnection(myconnstr);
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM tbl_kontakt WHERE imie LIKE '%"+keyword+ "%' OR nazwisko LIKE '%"+keyword+"%' OR telefon LIKE '%"+keyword+"%' OR adres LIKE '%"+keyword+"%' OR plec LIKE '%"+keyword+"%'", conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvListakontaktow.DataSource = dt;
+
+        }
+    }
+}
---
+++ Kontaktclass.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apkakontaktowa.Kontaktklasy
+{
+    class kontaktclass
+    {
+        public int kontaktID { get; set; }
+
+        public string imie { get; set; }
+
+        public string nazwisko { get; set; }
+
+        public string telefon { get; set; }
+
+        public string adres { get; set; }
+
+        public string plec { get; set; }
+
+        private string myconnstrng;
+
+        public kontaktclass(string cs)
+        {
+            myconnstrng = cs;
+        }
+
+        //wyciaganie z bazy danych
+        public DataTable Select()
+        {
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+
+                string sql = "SELECT * FROM tbl_kontakt";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+
+        //dodawanie do bazy danych
+
+        public bool dodaj()
+        {
+
+            bool isSuccess = false;
+
+
+            using (SqlConnection conn = new SqlConnection(myconnstrng))
+                try
+                {
+
+                    string sql = "INSERT INTO tbl_kontakt(imie, nazwisko,telefon, adres, plec) VALUES (@imie, @nazwisko,@telefon, @adres, @plec)";
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+
+                    cmd.Parameters.AddWithValue("@imie", imie);
+                    cmd.Parameters.AddWithValue("@nazwisko", nazwisko);
+                    cmd.Parameters.AddWithValue("@telefon", telefon);
+                    cmd.Parameters.AddWithValue("@adres", adres);
+                    cmd.Parameters.AddWithValue("@plec", plec);
+
+
+                    conn.Open();
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        isSuccess = true;
+                    }
+                    else
+                    {
+                        isSuccess = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            return isSuccess;
+        }
+
+        // aktualizacja bazy danych
+        public bool aktualizuj()
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+
+                string sql = "UPDATE tbl_kontakt SET imie=@imie, nazwisko=@nazwisko, telefon=@telefon, adres=@adres, plec=@plec WHERE kontaktID=@kontaktID";
+
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@imie", imie);
+                cmd.Parameters.AddWithValue("@nazwisko", nazwisko);
+                cmd.Parameters.AddWithValue("@telefon", telefon);
+                cmd.Parameters.AddWithValue("@adres", adres);
+                cmd.Parameters.AddWithValue("@plec", plec);
+                cmd.Parameters.AddWithValue("@kontaktID", kontaktID);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+
+        }
+        // usuwanie z bazy danych
+        public bool usun()
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "DELETE FROM tbl_kontakt WHERE kontaktID=@kontaktID";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@kontaktID", kontaktID);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+            return isSuccess;
+        }
+    }
+}
